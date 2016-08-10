c79cd08acad3c2b05f66bcdfa4d25b98d3875634
rtcatc <admin@rtcatc.com>
2016-08-10 15:54:40 +0800

    数据库

---
+++ ttylog.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version phpStudy 2014
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2016 年 08 月 10 日 15:50
+-- 服务器版本: 5.5.40
+-- PHP 版本: 5.3.29
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `kippo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ttylog`
+--
+
+CREATE TABLE IF NOT EXISTS `ttylog` (
+  `id` char(32) NOT NULL,
+  `ttylog` mediumblob NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=gbk;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
